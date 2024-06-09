25508f46f0846d74b316417acf895dbb0b029ed6
Md Mostafizar Rahman <136453073+mdfizar@users.noreply.github.com>
2024-06-09 19:02:43 +0600

    Add files via upload

---
+++ nfc-web/src/main.js
@@ -0,0 +1,28 @@
+// wthats new slider ==========
+var swiper = new Swiper(".whatsNew_slider", {
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+    },
+    571: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    991: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+
+   
+  },
+
+});
+
+// aos ======
+AOS.init();
