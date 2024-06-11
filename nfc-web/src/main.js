5b34b5b1e75bd0177b13091df79d5a6344134ef3
Md Mostafizar Rahman <136453073+mdfizar@users.noreply.github.com>
2024-06-11 19:13:54 +0600

    Add files via upload

--- nfc-web/src/main.js
+++ nfc-web/src/main.js
@@ -1,3 +1,33 @@
+//  language dropdown menu ======
+var dropdown = document.querySelector(".Dropdwon");
+var dropdonwItem = document.querySelector(".Dropdown-item");
+function handdleDropdown() {
+  dropdown.addEventListener('click', () => {
+    if (dropdonwItem.style.display === "none") {
+      dropdonwItem.style.display = "block";
+    } else {
+      dropdonwItem.style.display = "none";
+    }
+  });
+}
+  handdleDropdown(); 
+
+// product dropdown menu =====
+var dropdownproduct = document.querySelector(".product-dropdown");
+var dropdonwItemproduct = document.querySelector(".product-dropdown-item");
+function Dropdownproduct() {
+  dropdownproduct.addEventListener('click', () => {
+    if (dropdonwItemproduct.style.display === "none") {
+      dropdonwItemproduct.style.display = "block";
+    } else {
+      dropdonwItemproduct.style.display = "none";
+    }
+  });
+}
+  Dropdownproduct(); 
+
+
+
 // wthats new slider ==========
 var swiper = new Swiper(".whatsNew_slider", {
   loop: true,
@@ -26,3 +56,11 @@ var swiper = new Swiper(".whatsNew_slider", {
 
 // aos ======
 AOS.init();
+var faqItem = document.getElementById("faq-ans");
+ function faqItemHanddle () {
+  if(faqItem.style.height === "0px") {
+    faqItem.style.height ="100%";
+  } else {
+    faqItem.style.height ="0px";
+  }
+ }
