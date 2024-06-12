32285bb5908f448104d815dd67e98670bca5bad9
Md Mostafizar Rahman <136453073+mdfizar@users.noreply.github.com>
2024-06-12 19:25:39 +0600

    Add files via upload

---
+++ nfc-web - Copy/src/main.js
@@ -0,0 +1,66 @@
+// //  language dropdown menu ======
+// var dropdown = document.querySelector(".Dropdwon");
+// var dropdonwItem = document.querySelector(".Dropdown-item");
+// function handdleDropdown() {
+//   dropdown.addEventListener('click', () => {
+//     if (dropdonwItem.style.display === "none") {
+//       dropdonwItem.style.display = "block";
+//     } else {
+//       dropdonwItem.style.display = "none";
+//     }
+//   });
+// }
+//   handdleDropdown(); 
+
+// // product dropdown menu =====
+// var dropdownproduct = document.querySelector(".product-dropdown");
+// var dropdonwItemproduct = document.querySelector(".product-dropdown-item");
+// function Dropdownproduct() {
+//   dropdownproduct.addEventListener('click', () => {
+//     if (dropdonwItemproduct.style.display === "none") {
+//       dropdonwItemproduct.style.display = "block";
+//     } else {
+//       dropdonwItemproduct.style.display = "none";
+//     }
+//   });
+// }
+//   Dropdownproduct(); 
+
+
+
+// wthats new slider ==========
+var swiper = new Swiper(".whatsNew_slider", {
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+    },
+    571: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    991: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+
+   
+  },
+
+});
+
+// aos ======
+AOS.init();
+var faqItem = document.getElementById("faq-ans");
+ function faqItemHanddle () {
+  if(faqItem.style.height === "0px") {
+    faqItem.style.height ="100%";
+  } else {
+    faqItem.style.height ="0px";
+  }
+ }
